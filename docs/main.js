6338eb72d86f80ecb253cfed0172333007f0f302
Shinta Koyanagi <yancya@upec.jp>
2024-05-08 21:11:47 +0900

    とりあえずマップが出るようになった

---
+++ docs/main.js
@@ -0,0 +1,68 @@
+const f = g => {
+  var h, a, k;
+  var p = "The Google Maps JavaScript API";
+  var c = "google";
+  var l = "importLibrary";
+  var q = "__ib__";
+  var m = document;
+  var b = window;
+  b = b[c] || (b[c] = {});
+  var d = b.maps || ( b.maps={} );
+  var r = new Set;
+  var e = new URLSearchParams;
+  var u = ()=> h || (h = new Promise(async (f, n)=>{
+    await (a = m.createElement("script"));
+    e.set("libraries", [...r] + "");
+    for(k in g) {
+      e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]);
+    }
+    e.set("callback", c + ".maps." + q)
+    a.src = `https://maps.${c}apis.com/maps/api/js?` + e
+    d[q] = f
+    a.onerror=()=>h=n(Error(p+" could not load."))
+    a.nonce=m.querySelector("script[nonce]")?.nonce||""
+    m.head.append(a)
+  }))
+  d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f,...n) => r.add(f) && u().then(()=>d[l](f,...n));
+  console.log("Ready!")
+};
+f({ key: "AIzaSyAqprQv4t2EE8M4pp_Sb_4CEOfLoiWf77s", v: "beta" });
+
+const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+const { Map } = await google.maps.importLibrary("maps");
+
+// Initialize and add the map
+let map;
+
+async function plotPositions(map) {
+  const url = "https://storage.googleapis.com/yancya-club-bon-voyage/positions.json"
+  const by_visited_at = (a, b) => a.visited_at == b.visited_at ? 0 : (a.visited_at > b.visited_at ? 1 : -1);
+  fetch(url, { mode: "cors" }).then(response => response.json()).then(data => {
+    data.sort(by_visited_at).forEach((position, index) => {
+      const latLng = { lat: position.latitude, lng: position.longitude };
+      const footprint = document.createElement("img");
+      footprint.src = data.length == index+1 ? "./yancya.png" : "./footprint.png";
+      map.setCenter(latLng);
+      const marker = new AdvancedMarkerView({
+        map: map,
+        position: latLng,
+        content: footprint,
+        title: position.visited_at,
+      });
+      const delay = index * 0.5 + "s";
+      marker.element.style.setProperty("--delay-time", delay);
+      marker.element.style.opacity = ((index+1) / (data.length)) * 0.7 + 0.3
+    });
+  });
+}
+
+async function initMap() {
+  map = new Map(document.getElementById("main"), {
+    zoom: 4,
+    mapId: "BON_VOYAGE_YANCYA_CLUB",
+  });
+
+  await plotPositions(map);
+}
+
+initMap();
